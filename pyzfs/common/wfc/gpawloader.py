f0678a7ddb65e7ebc384f8d0986c844503f52b8b
Michael Toriyama <toriyama@midway3-login3.rcc.local>
2026-03-03 15:21:03 -0600

    GPAW plugin

--- pyzfs/common/wfc/gpawloader.py
+++ pyzfs/common/wfc/gpawloader.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import, division, print_function
 import numpy as np
 import os, sys
 from mpi4py import MPI
-from scipy.interpolate import interpn
 
 from .baseloader import WavefunctionLoader
 from ..cell import Cell
